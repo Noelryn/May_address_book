fb7f961d33013c97f7a83d30fdaf55fb5f0ea20b
Noelryn <noelgerard.ryan@gmail.com>
2020-06-01 16:49:38 +0200

    Writting tests for basic address book layout

---
+++ cypress/integration/userCanCreateACaontact.feature.js
@@ -0,0 +1,20 @@
+describe('user can create a contact', () => {
+  it('test', () =>{
+    cy.visit('http://localhost:3001')
+    cy.get('#add-contact').click()
+    cy.get('#name').type ('Noel')
+    cy.get('#email').type ('noel@email.com')
+    cy.get('#phone').type ('0976452623')
+    cy.get('#notes').type ('Is a coach at craft academy')
+    cy.get('#submit').click()
+  })
+
+  it('displays a name of the new contact', () => {
+    cy.get('#contact-list').should('contain', 'Noel')
+  })
+
+  it('displays a phone number of the new contact', () => {
+    cy.get('#contact-list').should('contain', '0976452623')
+  })
+
+})
